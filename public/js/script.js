7ff939133aba9febd20b75cb24c43673f878d70a
grovatron <grover.a.sundstrom@gmail.com>
2018-06-10 08:33:35 -0400

    Create operate function

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,21 @@
-function add(a,b) {
+let adding = false;
+let subtracting = false;
+let multiplying = false;
+let dividing = false;
+
+function operate(a, b) {
+    if (adding) {
+        return add(a, b);
+    } else if (subtracting) {
+        return subtract(a, b);
+    } else if (multiplying) {
+        return multiply(a, b);
+    } else if (dividing) {
+        return divide(a, b);
+    }
+}
+
+function add(a, b) {
     return a + b;
 }
 
